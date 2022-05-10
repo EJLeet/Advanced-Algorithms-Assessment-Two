407d8c51d93678b2592a28a6c7e0ca945c08159f
Ethan Leet <ethanjleet@gmail.com>
2022-05-10 17:43:56 +1000

    refactored

--- Question-2/main.cpp
+++ Question-2/main.cpp
@@ -5,28 +5,37 @@
 using std::cout;
 using std::endl;
 
-void generate(char* r, char* c);
+void generate(int r, int c);
 
 int main(int argc, char** argv)
 {
     if (argc < 3)
     { // ensure file passed
+
         cout << "ERROR! Expected Maze Dimensions Row and Col" << endl;
         exit(1);
     }
 
-    generate(argv[1], argv[2]);
+    int r = atoi(argv[1]), c = atoi(argv[2]);
+
+    if (r < 2 || c < 2)
+    {// ensure valid maze dimensions passed
+
+        cout << "ERROR! Minimum Size 2 x 2" << endl;
+        exit(1);
+    }
+
+    generate(r, c);
 
     return 0;
 }
 
-void generate(char* r, char* c)
+void generate(int r, int c)
 {
-    int row = atoi(r), col = atoi(c);
-    int size = row * col;
+    int size = r * c;
 
     // create maze
-    Maze maze (row, col);
+    Maze maze (r, c);
 
     // initialise maze with all walls in tact
     maze.initialise();
